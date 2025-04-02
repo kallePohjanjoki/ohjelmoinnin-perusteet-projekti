5809a36d72febb982944420d35bb4ded3f460160
Kalle Pohjanjoki <kalle@pohjanjoki.com>
2025-04-02 08:51:35 +0300

    Määrittele funktiot

---
+++ main.py
@@ -0,0 +1,15 @@
+with open("sanakirja.txt", "r", encoding="utf-8") as sanakirja:
+    content = sanakirja.read()
+
+def Search(word):
+    pass
+
+
+def Add(word):
+    pass
+
+def Modify(word):
+    pass
+
+def Delete(word):
+    pass
